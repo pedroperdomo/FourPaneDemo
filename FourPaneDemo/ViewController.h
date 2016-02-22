fbebfbdb907231306b5ad0f99ad2034e8902ed07
Pedro Pablo Perdomo Hernandez <pedropabloperdomohernandez@pedros-mac.local>
2016-02-22 20:01:53 +0000

    Initial Commit

---
+++ FourPaneDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FourPaneDemo
+//
+//  Created by Pedro Pablo Perdomo Hernandez on 22/2/16.
+//  Copyright © 2016 Pedro Pablo Perdomo Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FourPaneDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FourPaneDemo
+//
+//  Created by Pedro Pablo Perdomo Hernandez on 22/2/16.
+//  Copyright © 2016 Pedro Pablo Perdomo Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
